466298f7ae95119d93d181a56506cbd945718337
mufasa-dev <bruno.ribeiro96@hotmail.com.br>
2025-04-14 09:08:55 -0300

    Update redux and testing

--- src/routes.js
+++ src/routes.js
@@ -3,19 +3,18 @@ import React from 'react';
 import PokeDex from './pages/PokeDex/PokeDex';
 import PokeList from './pages/PokeList/PokeList';
 
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 
-import { BrowserRouter, Route, Routes, Redirect} from 'react-router-dom';
- 
 import { createBrowserHistory  } from 'history';
 
 const history = createBrowserHistory();
 
 const MainRoutes = () => (
     <BrowserRouter>
-        <Routes >
+        <Routes>
             <Route exact path="/" element={<PokeList history={history} />} />
             <Route path="/list" element={<PokeList history={history} />} />
-            <Route path="/pokedex" element={<PokeDex history={history} />} /> 
+            <Route path="/pokedex" element={<PokeDex history={history} />} />
         </Routes>
     </BrowserRouter>
 )
