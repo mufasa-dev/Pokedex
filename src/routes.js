6ac2a9130d25bd9e3fda0de5e941e32366e3455f
mufasa-dev <bruno.ribeiro96@hotmail.com.br>
2025-04-18 13:32:05 -0300

    change to hashRouter

--- src/routes.js
+++ src/routes.js
@@ -3,20 +3,20 @@ import React from 'react';
 import PokeDex from './pages/PokeDex/PokeDex';
 import PokeList from './pages/PokeList/PokeList';
 
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import { HashRouter, Route, Routes} from 'react-router-dom';
 
 import { createBrowserHistory  } from 'history';
 
 const history = createBrowserHistory();
 
 const MainRoutes = () => (
-    <BrowserRouter>
+    <HashRouter>
         <Routes>
             <Route exact path="/" element={<PokeList history={history} />} />
             <Route path="/list" element={<PokeList history={history} />} />
             <Route path="/pokedex" element={<PokeDex history={history} />} />
         </Routes>
-    </BrowserRouter>
+    </HashRouter>
 )
 
 export default MainRoutes;
