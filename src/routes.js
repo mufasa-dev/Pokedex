6c22e9abe369bca90e3d1e3c251bf0a2dd4ba269
mufasa-dev <bruno.ribeiro96@hotmail.com.br>
2025-04-19 00:38:06 -0300

    return to borwser routes

--- src/routes.js
+++ src/routes.js
@@ -3,20 +3,20 @@ import React from 'react';
 import PokeDex from './pages/PokeDex/PokeDex';
 import PokeList from './pages/PokeList/PokeList';
 
-import { HashRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 
 import { createBrowserHistory  } from 'history';
 
 const history = createBrowserHistory();
 
 const MainRoutes = () => (
-    <HashRouter>
+    <BrowserRouter>
         <Routes>
             <Route exact path="/" element={<PokeList history={history} />} />
             <Route path="/list" element={<PokeList history={history} />} />
             <Route path="/pokedex" element={<PokeDex history={history} />} />
         </Routes>
-    </HashRouter>
+    </BrowserRouter>
 )
 
 export default MainRoutes;
